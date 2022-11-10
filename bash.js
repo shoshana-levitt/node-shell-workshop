92a21cf0df6f0323ac4dbdaa8d6e69b576b6d177
Shoshana Levitt <shoshana.levitt@gmail.com>
2022-11-10 15:21:10 -0500

    commit-1

--- bash.js
+++ bash.js
@@ -4,7 +4,7 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim(); //remove the newline
-  console.log(process.env.PWD);
+  console.log(process.env[PWD]);
   process.stdout.write('You typed: ' + cmd);
   process.stdout.write('\nprompt > ');
 });
